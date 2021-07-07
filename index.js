cc5ba03099659d3b88439905903b3bd85cbe02e2
Random <jah@pop-os.localdomain>
2021-07-07 15:38:42 -0500

    Count up & down & change text

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+var count = 0;
+
+document.getElementById('countup').addEventListener("click", () => {
+    count++;
+
+    document.getElementById('counter').innerHTML = count;
+});
+
+document.getElementById('countdown').addEventListener("click", () => {
+    count--;
+
+    document.getElementById('counter').innerHTML = count;
+});
